18d03d445abdae91bbc02f8341e16739ec4ecd8b
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-25 08:05:27 -0800

    commit tree 81e1f9378bf9ed6da4823367a2ca0361e04dbff5

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'brown', 'sad', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'F', 'orange', 'riled-up', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'white', 'hungry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'M', 'black', 'awesome', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 13, 'F', 'red', 'open-minded', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'green', 'jerk', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'white', 'awesome', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NUll, 2, 'F', 'purple', NULL, 1);
